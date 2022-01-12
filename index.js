137b3277a547d3136889c4fd6e307cf29ac9315c
omairapalacios <cpalaciosanchez@hotmail.com>
2022-01-11 23:18:06 -0500

    add router users

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users')
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL,
-  { useNewUrlParser: true , useUnifiedTopology: true }
+  { useNewUrlParser: true, useUnifiedTopology: true }
   , () => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
+
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute)
 
 app.listen(4000, () => {
   console.log('Server is running');
