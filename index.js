1466c2ae79ec2931e888b29ab4e9b3053d7e2ba6
omairapalacios <cpalaciosanchez@hotmail.com>
2022-01-11 22:54:52 -0500

    add conecction to mongo atlas

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,
+  { useNewUrlParser: true , useUnifiedTopology: true }
+  , () => {
+    console.log("Connected to MongoDB");
+  })
+
 app.listen(4000, () => {
   console.log('Server is running');
 })
