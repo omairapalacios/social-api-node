6f2918ca8b64a5e8699cb09b5690cab5ff339343
omairapalacios <cpalaciosanchez@hotmail.com>
2022-01-13 23:58:53 -0500

    add ruta register

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const User = require('../models/User');
+
+router.post('/register', (req, res) => {
+  const user = new User({})
+  user.save()
+  res.send('Hey! auth')
+})
 
 router.get('/', (req, res) => {
   res.send('Hey! auth')
